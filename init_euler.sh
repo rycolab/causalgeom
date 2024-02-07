58a297b6caa5621e34b47b3eaab9159106acfcf2
cguerner <clement.guerner@gmail.com>
2024-02-07 14:29:20 +0100

    fixed stuff to make presentable to luca

--- init_euler.sh
+++ init_euler.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-module load eth_proxy gcc/8.2.0 python_gpu/3.10.4
-
-eval "$(ssh-agent -s)"
-ssh-add $HOME/.ssh/id_ed25519_euler_remote
+module load eth_proxy gcc/8.2.0 python_gpu/3.11.2
 
 if [ ! -d "venv/" ]; then
     python3 -m venv venv
-    echo "Created virtual environment."
+    echo "Created virtual environment"
 fi
 
-export TA_CACHE_DIR="/scratch/$USER/.cache"
-
 source venv/bin/activate
+python -m pip install -r requirements.txt 
+echo "Installed requirements"
 #pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
