020b7022166b4844c211e9b4f3a98e023da82d3e
Tianyu Liu <tianyu.liu@inf.ethz.ch>
2024-05-14 12:36:51 +0200

    my path

--- src/paths.py
+++ src/paths.py
@@ -1,6 +1,6 @@
 import os
 
-MAIN = "/cluster/work/cotterell/cguerner/usagebasedprobing/"
+MAIN = "/cluster/home/tialiu/tianyu_c/usagebasedprobing/"
 
 OUT = os.path.join(MAIN, "out")
 DATASETS = os.path.join(MAIN, "data")
@@ -15,10 +15,10 @@ BERT_SYNTAX_LINZEN_GOLDBERG = "../bert-syntax/lgd_dataset.tsv"
 LINZEN_RAW = os.path.join(DATASETS, "raw/linzen/agr_50_mostcommon_10k.tsv")
 LINZEN_VOCAB = os.path.join(DATASETS, "raw/linzen/wiki.vocab")
 
-UNIMORPH_ENG = "/cluster/work/cotterell/cguerner/eng/eng"
+UNIMORPH_ENG = "/cluster/home/tialiu/tianyu_c/eng/eng"
 
-UD_FRENCH_GSD = "/cluster/work/cotterell/cguerner/UD_French-GSD"
-UD_FRENCH_ParTUT = "/cluster/work/cotterell/cguerner/UD_French-ParTUT"
-UD_FRENCH_Rhapsodie = "/cluster/work/cotterell/cguerner/UD_French-Rhapsodie"
+UD_FRENCH_GSD = "/cluster/home/tialiu/tianyu_c/UD_French-GSD"
+UD_FRENCH_ParTUT = "/cluster/home/tialiu/tianyu_c/UD_French-ParTUT"
+UD_FRENCH_Rhapsodie = "/cluster/home/tialiu/tianyu_c/UD_French-Rhapsodie"
 
 FR_DATASETS = ["ud_fr_gsd", "ud_fr_partut", "ud_fr_rhapsodie"]
