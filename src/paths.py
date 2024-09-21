838272d8d4f50a23f126e0e8cfd00c28073fae3d
cguerner <clement.guerner@gmail.com>
2024-09-21 15:00:27 +0200

    removed paths

--- src/paths.py
+++ src/paths.py
@@ -1,23 +1,32 @@
 import os
 
-MAIN = "/cluster/work/cotterell/cguerner/usagebasedprobing/"
+# ONBOARDING STEPS:
+# 1. Set MAIN path to the folder where you cloned the repository
+MAIN = ""
 OUT = os.path.join(MAIN, "out")
 DATASETS = os.path.join(MAIN, "data")
 RESULTS = os.path.join(MAIN, "results")
 MODELS = os.path.join(MAIN, "models")
 HF_CACHE = "../hf_cache"
+
+# 2. Set the path to your huggingface authentication token
 AUTH_TOKEN_PATH = os.path.join(MAIN, "auth_token.txt")
 
-
-#BERT_SYNTAX_LINZEN_GOLDBERG = "../bert-syntax/lgd_dataset.tsv"
-
+# 3. Set paths to where you downloaded the raw Linzen data files
 LINZEN_RAW = os.path.join(DATASETS, "raw/linzen/agr_50_mostcommon_10k.tsv")
 LINZEN_VOCAB = os.path.join(DATASETS, "raw/linzen/wiki.vocab")
 
-#UNIMORPH_ENG = "/cluster/work/cotterell/cguerner/eng/eng"
+# 4. Set paths to where you cloned the TreeBank repositories
+UD_REPOS_MAIN = ""
+UD_FRENCH_GSD = os.path.join(
+    UD_REPOS_MAIN, "UD_French-GSD"
+)
+UD_FRENCH_ParTUT = os.path.join(
+    UD_REPOS_MAIN, "UD_French-ParTUT"
+)
+UD_FRENCH_Rhapsodie = os.path.join(
+    UD_REPOS_MAIN, "UD_French-Rhapsodie"
+)
 
-UD_FRENCH_GSD = "/cluster/work/cotterell/cguerner/UD_French-GSD"
-UD_FRENCH_ParTUT = "/cluster/work/cotterell/cguerner/UD_French-ParTUT"
-UD_FRENCH_Rhapsodie = "/cluster/work/cotterell/cguerner/UD_French-Rhapsodie"
-
+# Don't modify this list below, unless you want to add a FR Treebank
 FR_DATASETS = ["ud_fr_gsd", "ud_fr_partut", "ud_fr_rhapsodie"]
